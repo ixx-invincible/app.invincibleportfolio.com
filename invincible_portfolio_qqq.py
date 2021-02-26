7ef225b91055794ceaccd269e6aa76d5bbfbc9ad
Terry Law <tl@ixx.cc>
2021-02-26 12:05:19 +0800

    added mtum qqq portfolio

--- invincible_portfolio_qqq.py
+++ invincible_portfolio_qqq.py
@@ -15,17 +15,17 @@ import matplotlib.ticker as mtick
 import matplotlib.gridspec as gridspec
 
 
-# prices = ffn.get('GLD, SPY, TLT', start='2005-01-01')
-prices = ffn.get('GLD, SPY, TLT', start='2014-01-01')
+# prices = ffn.get('GLD, MTUM, TLT', start='2005-01-01')
+prices = ffn.get('GLD, QQQ, TLT', start='2014-01-01')
 
 prices = prices.reset_index()
 prices['portfolio'] = 100
 prices['gld_rb'] = 0
-prices['spy_rb'] = 0
+prices['qqq_rb'] = 0
 prices['tlt_rb'] = 0
 prices['portfolio_rb'] = 100
 
-symbols = ['gld', 'spy', 'tlt']
+symbols = ['gld', 'qqq', 'tlt']
 
 
 for i in prices.index:
@@ -36,7 +36,7 @@ for i in prices.index:
         prices.loc[i, 'portfolio_rb'] = 100
         
     else:
-        prices.loc[i, 'portfolio'] = prices['portfolio_rb'][i-1] * ((prices['spy'][i] / prices['spy_rb'][i-1] + prices['tlt'][i] / prices['tlt_rb'][i-1] + prices['gld'][i] / prices['gld_rb'][i-1]) / 3)
+        prices.loc[i, 'portfolio'] = prices['portfolio_rb'][i-1] * ((prices['qqq'][i] / prices['qqq_rb'][i-1] + prices['tlt'][i] / prices['tlt_rb'][i-1] + prices['gld'][i] / prices['gld_rb'][i-1]) / 3)
         
         if(i != len(prices)-1 and prices.Date[i].month % 3 == 0 and prices.Date[i+1].month % 3 == 1):
             for symbol in symbols:
@@ -55,10 +55,10 @@ for i in prices.index:
 weekday = datetime.today().weekday()
 
 
-prices.to_csv('out/invincible_portfolio_' + str(weekday) + '.csv')
-prices.to_csv('quote/invincible_portfolio.csv')
-prices.to_json('quote/invincible_portfolio.json', orient='index')
-prices.tail(2).to_json('quote/invincible_portfolio_latest.json', orient='records')
+prices.to_csv('out/invincible_portfolio_qqq_' + str(weekday) + '.csv')
+prices.to_csv('quote/invincible_portfolio_qqq.csv')
+prices.to_json('quote/invincible_portfolio_qqq.json', orient='index')
+prices.tail(2).to_json('quote/invincible_portfolio_qqq_latest.json', orient='records')
 
 
 prices.set_index('Date', inplace=True)
@@ -66,7 +66,7 @@ prices.set_index('Date', inplace=True)
 # returns = prices.loc[:, ['spy', 'gld', 'tlt', 'portfolio']].to_returns().dropna()
 # print(returns.corr().as_format('.2f'))
 
-perf = prices.loc[:, ['spy', 'gld', 'tlt', 'portfolio']].calc_stats()
+perf = prices.loc[:, ['qqq', 'gld', 'tlt', 'portfolio']].calc_stats()
 
                 
 
@@ -74,7 +74,7 @@ fig = plt.figure(constrained_layout=True, figsize=(10, 5))
 spec = fig.add_gridspec(ncols=1, nrows=2, height_ratios=[3, 1])
 
 ax1 = fig.add_subplot(spec[0, 0])
-ax1.plot(prices['portfolio'], label='Portfolio', linewidth=2)
+ax1.plot(prices['portfolio'], label='Portfolio (QQQ)', linewidth=2)
 ax1.legend(loc='upper left')
 ax1.grid(True)
 
@@ -85,7 +85,7 @@ ax2.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
 ax2.grid(True)
 
 
-plt.savefig('out/invincible_portfolio_' + str(weekday) + '.png')
+plt.savefig('out/invincible_portfolio_qqq_' + str(weekday) + '.png')
 plt.close()
 
 
@@ -97,8 +97,8 @@ for symbol in symbols:
     perf[symbol].return_table.to_json('quote/' + symbol + '.json', orient='index')
 
 
-perf['portfolio'].stats.to_csv('out/invincible_portfolio_stats_' + str(weekday) + '.csv')
-perf['portfolio'].stats.to_json('quote/invincible_portfolio_stats.json')
-perf['portfolio'].return_table.to_csv('out/invincible_portfolio_monthly_returns_' + str(weekday) + '.csv')
-perf['portfolio'].return_table.to_json('quote/invincible_portfolio_monthly_returns.json', orient='index')
+perf['portfolio'].stats.to_csv('out/invincible_portfolio_qqq_stats_' + str(weekday) + '.csv')
+perf['portfolio'].stats.to_json('quote/invincible_portfolio_qqq_stats.json')
+perf['portfolio'].return_table.to_csv('out/invincible_portfolio_qqq_monthly_returns_' + str(weekday) + '.csv')
+perf['portfolio'].return_table.to_json('quote/invincible_portfolio_qqq_monthly_returns.json', orient='index')
 
